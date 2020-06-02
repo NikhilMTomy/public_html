382cee120f07e4d945fad431443b97fcf7718455
Stephenthomas017 <62503790+Stephenthomas017@users.noreply.github.com>
2020-06-02 16:29:52 +0530

    Added contact page html

---
+++ contacts.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title>contact</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+
+    </header>
+    <body>
+        <div class="contact-section">
+            
+            <div class="inner-width">
+                <h1>Get in touch with us</h1>
+                <input type="text" class="name" placeholder="Your Name">
+                <input type="text" class="email" placeholder="Email">
+                <textarea rows="1" class="message" placeholder="Message"></textarea>
+                <button>Get in Touch</button>
+
+            </div>
+        </div>
+    </body>
+</html>
