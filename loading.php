3c65ba5455b4fd56d660dc1d629ef9d09198b1cb
Stephenthomas017 <62503790+Stephenthomas017@users.noreply.github.com>
2020-06-06 10:49:55 +0530

    Added loading html file

---
+++ loading.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>loading</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="loading.css">
+
+    </head>
+    <body>
+        <div class="loading">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+    </body>
+</html>
